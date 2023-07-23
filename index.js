90ba6902af7b64e7836d98b28f606080110becaf
Imad Eddine Kettaf <imedkettaf@gmail.com>
2023-07-23 22:52:09 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const distanceFromHqInBlocks = destination => {
+    return Math.abs(42 - destination);
+};
+
+const distanceFromHqInFeet = destination => {
+    return distanceFromHqInBlocks(destination) * 264;
+};
+
+const distanceTravelledInFeet = (start, destination) => {
+    return distanceFromHqInFeet(destination) - distanceFromHqInFeet(start);
+};
+
+const calculatesFarePrice = (start, destination) => {
+    const travelledDistance = distanceTravelledInFeet(start, destination);
+    
+    if (travelledDistance <= 400)
+        return 0;
+    if (travelledDistance > 400 && travelledDistance <= 2000)
+        return (travelledDistance - 400) * 0.02;
+    if (travelledDistance > 2000 && travelledDistance <= 2500)
+        return 25;
+
+    return 'cannot travel that far';
+};
